85f35e1470e5c5e757cfb354cef3ce0e870038c2
Nerina-fran <60006145+Nerina-fran@users.noreply.github.com>
2022-11-07 12:34:46 -0500

    add to info the user.php

--- db/conn.php
+++ db/conn.php
@@ -26,7 +26,10 @@
         throw new PDOException($e->getMessage());
     }
 
-    require_once'crud.php';
+    require_once 'crud.php';
+    require_once 'user.php';
     $crud = new crud($pdo);
+    $user = new user($pdo);
 
+    $user->insertUser("admin","password");
 ?>
--- db/user.php
+++ db/user.php
@@ -9,38 +9,60 @@
         function __construct($conn){
             $this->db = $conn;
         }
-    }
+    
 
-    public function insertUser($username,$password){
-        try {
-            // define sql statement to be executed
-            $sql = "INSERT INTO users (username, password) VALUES(:username, :password)";
-            // prepare sql statement for execution
-            $stmt = $this->db->prepare($sql);
-            // bind all placeholders to the actual values
-            $stmt->bindparam(':username',$username);
-            $stmt->bindparam(':password',$password);
-            
-            // excute statement
-            $stmt->execute();
-            return true;
+        public function insertUser($username,$password){
+            try {
+                $result = $this->getUserbyUsername($username);
+                if($result['num']> 0){
+                    return false;
+                } else{
+                    $new_password = md5($password. $username);
+                    // define sql statement to be executed
+                    $sql = "INSERT INTO users (username, password) VALUES(:username,:password)";
+                    // prepare sql statement for execution
+                    $stmt = $this->db->prepare($sql);
+                    // bind all placeholders to the actual values
+                    $stmt->bindparam(':username',$username);
+                    $stmt->bindparam(':password',$new_password);
+                
+                    // excute statement
+                    $stmt->execute();
+                    return true;
+                }
 
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-            return false;
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+                return false;
+            }
+        } 
+        public function getUser($username,$password){
+            try{
+                $sql = "select * from users where username = :username AND password = :password";
+                $stmt = $this->db->prepare($sql);
+                $stmt->bindparam('username', $username);
+                $stmt->bindparam(':password', $password);
+                $stmt->execute();
+                $result = $stmt->fetch();
+                return $result;
+            }catch (PDOException $e) {
+                echo $e->getMessage();
+                return false;
+            }
         }
-    } 
-    public function getuser($username,$password){
-        try{
-            $sql = "select * from users where username = username AND password" = :password";
-            $stmt = $this->db->prepare($sql);
-            $stmt->bindparam(':id', $id);
-            $stmt->execute();
-            $result = $stmt->fetch();
-            return $result;
-        }catch (PDOException $e) {
-            echo $e->getMessage();
-            return false;
+
+        public function getUserbyUsername($username){
+            try{
+                $sql = "select count(*) as num from users where username = :username";
+                $stmt = $this->db->prepare($sql);
+                $stmt->bindparam('username', $username);
+                $stmt->execute();
+                $result = $stmt->fetch();
+                return $result;
+            }catch (PDOException $e) {
+                echo $e->getMessage();
+                return false;
+            }
         }
-    }
+    }    
 ?>
